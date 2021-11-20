78e71fa0e1af347ed754f122c7e5b2181f00ae7d
Daniel Inkpen <dan2552@gmail.com>
2021-11-20 17:53:20 +0000

    child view

--- examples/ui.rs
+++ examples/ui.rs
@@ -1,5 +1,5 @@
-use pelican::graphics::{Rectangle, Point, Size};
-use pelican::ui::{View, Window};
+use pelican::graphics::{Rectangle};
+use pelican::ui::{View, Window, Color};
 use pelican::ui::{ApplicationMain, ApplicationDelegate};
 use pelican::ui::{ViewController, ViewControllerBehavior};
 
@@ -17,18 +17,19 @@ impl ViewControllerBehavior for ExampleViewController {
     fn view_did_appear(&self, _view: View) {
         println!("view_did_appear");
     }
-    fn view_did_load(&self, _view: View) {
+    fn view_did_load(&self, view: View) {
         println!("view_did_load");
+        let frame = Rectangle::new(10, 10, 30, 30);
+        let child_view = View::new(frame);
+        child_view.set_background_color(Color::red());
+        view.add_subview(child_view);
     }
 }
 
 struct AppDelegate {}
 impl ApplicationDelegate for AppDelegate {
     fn application_did_finish_launching(&self) {
-        let frame = Rectangle {
-            position: Point { x: 10, y: 10 },
-            size: Size { width: 480, height: 320 }
-        };
+        let frame = Rectangle::new(10, 10, 480, 320);
 
         // TODO: is it possible to make it so you just pass in
         // ExampleViewController and it gets automatically coerced to the
--- src/graphics/rectangle.rs
+++ src/graphics/rectangle.rs
@@ -3,60 +3,69 @@ use crate::graphics::Size;
 use std::ops::Mul;
 
 pub struct Rectangle<T, U> {
-  pub position: Point<T>,
-  pub size: Size<U>
+    pub position: Point<T>,
+    pub size: Size<U>
+}
+
+impl<T, U> Rectangle<T, U> {
+    pub fn new(x: T, y: T, width: U, height: U) -> Self {
+        Rectangle {
+            position: Point { x, y },
+            size: Size { width, height }
+        }
+    }
 }
 
 impl<T, U> Clone for Rectangle<T, U> where T: Copy, U: Copy {
-  fn clone(&self) -> Self {
-    Rectangle {
-      position: self.position.clone(),
-      size: self.size.clone()
+    fn clone(&self) -> Self {
+        Rectangle {
+            position: self.position.clone(),
+            size: self.size.clone()
+        }
     }
-  }
 }
 
 impl Mul<f32> for &Rectangle<i32, i32> {
     type Output = Rectangle<i32, i32>;
     fn mul(self, rhs: f32) -> Self::Output {
-      let x = self.position.x as f32 * rhs;
-      let y = self.position.y as f32 * rhs;
-      let width = self.size.width as f32 * rhs;
-      let height = self.size.height as f32 * rhs;
+        let x = self.position.x as f32 * rhs;
+        let y = self.position.y as f32 * rhs;
+        let width = self.size.width as f32 * rhs;
+        let height = self.size.height as f32 * rhs;
 
-      Rectangle {
+        Rectangle {
         position: Point { x: x.round() as i32, y: y.round() as i32 },
         size: Size { width: width.round() as i32, height: height.round() as i32 }
-      }
+        }
     }
 }
 
 impl Mul<f32> for &Rectangle<i32, u32> {
   type Output = Rectangle<i32, u32>;
   fn mul(self, rhs: f32) -> Self::Output {
-    let x = self.position.x as f32 * rhs;
-    let y = self.position.y as f32 * rhs;
-    let width = self.size.width as f32 * rhs;
-    let height = self.size.height as f32 * rhs;
+        let x = self.position.x as f32 * rhs;
+        let y = self.position.y as f32 * rhs;
+        let width = self.size.width as f32 * rhs;
+        let height = self.size.height as f32 * rhs;
 
-    Rectangle {
-      position: Point { x: x.round() as i32, y: y.round() as i32 },
-      size: Size { width: width.round() as u32, height: height.round() as u32 }
-    }
+        Rectangle {
+            position: Point { x: x.round() as i32, y: y.round() as i32 },
+            size: Size { width: width.round() as u32, height: height.round() as u32 }
+        }
   }
 }
 
 impl Mul<f32> for &Rectangle<f32, f32> {
-  type Output = Rectangle<f32, f32>;
-  fn mul(self, rhs: f32) -> Self::Output {
-    let x = self.position.x * rhs;
-    let y = self.position.y * rhs;
-    let width = self.size.width * rhs;
-    let height = self.size.height * rhs;
+    type Output = Rectangle<f32, f32>;
+    fn mul(self, rhs: f32) -> Self::Output {
+        let x = self.position.x * rhs;
+        let y = self.position.y * rhs;
+        let width = self.size.width * rhs;
+        let height = self.size.height * rhs;
 
-    Rectangle {
-      position: Point { x: x, y: y },
-      size: Size { width: width, height: height }
+        Rectangle {
+            position: Point { x: x, y: y },
+            size: Size { width: width, height: height }
+        }
     }
-  }
 }
