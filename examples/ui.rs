7673059b625475752941e486c9306c0e61654f61
Daniel Inkpen <dan2552@gmail.com>
2021-12-08 01:08:26 +0000

    Fix ui example

--- examples/ui.rs
+++ examples/ui.rs
@@ -34,7 +34,9 @@ impl ViewControllerBehavior for ExampleViewController {
         blue_view.set_background_color(Color::new(0, 0, 255, 100));
 
         let frame = Rectangle::new(200, 200, 200, 200);
-        let button = Button::new(frame, "this is a button");
+        let button = Button::new(frame, "this is a button", || {
+            println!("button clicked");
+        });
 
         // view.add_subview(red_view);
         // view.add_subview(green_view);
