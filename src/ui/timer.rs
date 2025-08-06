bd432c967c75dd7004c544bb45251a4419a9b35e
Daniel Inkpen <dan2552@gmail.com>
2025-08-06 23:24:07 +0100

    Revert "Put timer back to single thread, as timer's inner action will always be main thread"
    This reverts commit 496d9d8a73f418ce4332d569922d7882fa119dde.

--- src/ui/timer.rs
+++ src/ui/timer.rs
@@ -1,11 +1,11 @@
+use std::sync::Arc;
+use std::sync::RwLock;
 use std::time::Duration;
 use std::time::Instant;
-use std::cell::Cell;
-use std::rc::Rc;
 
 // A repeating or once-off Timer object, to be run by the main loop.
 pub struct Timer {
-    inner: Rc<TimerInner>
+    inner: Arc<RwLock<TimerInner>>
 }
 
 struct TimerInner {
@@ -16,31 +16,31 @@ struct TimerInner {
     // If set to invalid, the timer will no longer run, and the main loop will
     // recognise it should be removed. In addition, an invalid timer cannot be
     // reused; it cannot be made valid again.
-    is_valid: Cell<bool>,
+    is_valid: bool,
 
     // The last time the timer has fired. If it hasn't fired, it'll be the
     // time of initialization.
-    last_fired_at: Cell<Instant>,
+    last_fired_at: Instant,
 
     // Next target time to fire. If there is a delay to fire, or the fire takes
     // too long, it
     //
     // When invalidated, the last fire date.
-    fire_at: Cell<Instant>
+    fire_at: Instant
 }
 
 impl Timer {
     pub fn new(interval: Duration, repeats: bool, action: impl Fn() -> () + 'static) -> Self {
         let now = Instant::now();
         Self {
-            inner: Rc::new(TimerInner {
+            inner: Arc::new(RwLock::new(TimerInner {
                 interval,
                 repeats,
                 action: Box::new(action),
-                is_valid: Cell::new(true),
-                last_fired_at: Cell::new(now),
-                fire_at: Cell::new(now + interval)
-            })
+                is_valid: true,
+                last_fired_at: now,
+                fire_at: now + interval
+            }))
         }
     }
 
@@ -57,32 +57,45 @@ impl Timer {
     }
 
     // Run the action
-    pub(crate) fn fire(&self) {
+    pub(crate) fn fire(&mut self) {
         let current_fire_at = Instant::now();
-        (self.inner.action)();
+        let mut inner = self.inner
+            .write()
+            .expect("Failed to write timer state");
+        (inner.action)();
 
-        self.inner.fire_at.set(current_fire_at + self.inner.interval);
-        self.inner.last_fired_at.set(current_fire_at);
+        inner.fire_at = current_fire_at + inner.interval;
+        inner.last_fired_at = current_fire_at;
 
-        if !self.inner.repeats {
-            self.inner.is_valid.set(false)
+        if !inner.repeats {
+            inner.is_valid = false;
         }
     }
 
     pub fn is_valid(&self) -> bool {
-        self.inner.is_valid.get()
+        self.inner
+            .read()
+            .expect("Failed to read timer state")
+            .is_valid
     }
 
     pub fn fire_at(&self) -> Instant {
-        self.inner.fire_at.get().clone()
+        self.inner
+            .read()
+            .expect("Failed to read timer state")
+            .fire_at
     }
 
     // Sets the timer as invalidated. Meaning the run loop will recognise this:
     // * To not fire
     // * To be removed from the run loop
-    pub fn invalidate(&self) {
-        self.inner.is_valid.set(false);
-        self.inner.fire_at.set(self.inner.last_fired_at.get());
+    pub fn invalidate(&mut self) {
+        let mut inner = self.inner
+            .write()
+            .expect("Failed to write timer state");
+
+        inner.is_valid = false;
+        inner.fire_at = inner.last_fired_at.clone();
     }
 }
 
@@ -104,7 +117,7 @@ mod tests {
         //   it returns the next time to fire (now + given interval)
         let now = Instant::now();
         let interval = Duration::from_secs(360);
-        let timer = Timer::new(interval, false, || {});
+        let mut timer = Timer::new(interval, false, || {});
 
         let fire_at = timer.fire_at();
 
@@ -141,7 +154,7 @@ mod tests {
     #[test]
     fn test_is_valid() {
         let interval = Duration::from_secs(360);
-        let timer = Timer::new(interval, false, || {});
+        let mut timer = Timer::new(interval, false, || {});
 
         assert!(timer.is_valid());
         timer.invalidate();
@@ -154,7 +167,7 @@ mod tests {
     fn test_fire() {
         let interval = Duration::from_secs(360);
 
-        let timer = Timer::new(interval, false, || {
+        let mut timer = Timer::new(interval, false, || {
             unsafe { FIRED = true; }
         });
 
